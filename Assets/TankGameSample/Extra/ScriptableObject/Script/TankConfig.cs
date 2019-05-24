7241172849e02b50f4a42bf5f94a42b0be4aada5
MizoTake <mizotake123@gmail.com>
2019-05-25 01:25:59 +0900

    update extra folder

---
+++ Assets/TankGameSample/Extra/ScriptableObject/Script/TankConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankConfig : ScriptableObject
+{
+    public int hp = 3;
+    public float speed = 1;
+    public float firePower = 1;
+    public GameObject bulletPrefab;
+}
