73c2946cabdff62f19402fb7560c82a749ff4f13
MizoTake <mizotake123@gmail.com>
2019-05-24 21:17:01 +0900

    add sample scene process

---
+++ Assets/TankGameSample/Extra/AutoSpawn/Script/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject SpawnTargetPrefab;
+    
+    private float spawnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer > 3.0f) {
+            Instantiate(SpawnTargetPrefab, transform.position + Vector3.down * 3f, Quaternion.identity);
+            spawnTimer = 0;
+        }
+    }
+}
